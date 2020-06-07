0103820fc336b20fa44d72d80d61466d7990ebc7
CHAHATMB <chahatmbaghele@gmail.com>
2020-06-07 11:10:56 +0530

    I have create a login page and register page

---
+++ Chahat/my/models.py
@@ -0,0 +1,21 @@
+from __init__ import db, login_manager, app
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    
+
+    def __repr__(self):
+        return f"User('{ self.first_name }','{ self.last_name }',{self.email}', '{self.image_file}')"
+
