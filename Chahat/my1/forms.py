db7b51e6dabbbf247ef8d2bd17cf6d0c14069051
CHAHATMB <chahatmbaghele@gmail.com>
2020-07-05 15:44:28 +0530

    search done

---
+++ Chahat/my1/forms.py
@@ -0,0 +1,49 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+
+
+class SearchBar(FlaskForm):
+    search_query = StringField('Enter Your Hobby', validators=[DataRequired()])
+    submit = SubmitField('Search')
+
+class HobbiesForm(FlaskForm):
+    tag = StringField("Tag", validators=[DataRequired()])
+    title = StringField("Tittle", validators=[DataRequired()])
+    content = StringField("Discription", validators=[DataRequired()])
+    picture = FileField('Add a Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Submit')
---
+++ Chahat/my1/models.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from __init__ import db, login_manager, app
+from flask_login import UserMixin
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+subs = db.Table('subs',
+db.Column('user_id',db.Integer, db.ForeignKey('user.user_id')),
+db.Column('prod_id',db.Integer,db.ForeignKey('hobies.prod_id'))
+)
+
+
+class User(db.Model, UserMixin):
+    user_id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def get_id(self):
+        return self.user_id
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
+    def __repr__(self):
+        return f"User('{ self.first_name }','{ self.last_name }',{self.email}', '{self.image_file}')"
+
+
+
+class Hobies(db.Model):
+    __tablename__ = 'hobies'
+    prod_id = db.Column(db.Integer, primary_key=True)
+    tag = db.Column(db.String(50), nullable=False)  
+    title = db.Column(db.String(50), nullable=False)  
+    content = db.Column(db.String(200), nullable=False)   
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    users_prod = db.relationship('User',secondary=subs, backref=db.backref('products',lazy='dynamic'))
